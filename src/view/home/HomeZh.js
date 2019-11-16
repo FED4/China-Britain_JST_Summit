6e45c53222f90fff12595075509e9179d6fd8c08
Antimony51122 <zhuyaosheng51122@gmail.com>
2019-11-16 00:24:54 +0000

    5 cent home page

--- src/view/home/HomeZh.js
+++ src/view/home/HomeZh.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./home.css";
 //import logo from './logo.svg';
 
@@ -6,24 +7,47 @@ import "./home.css";
 /* eslint-disable */
 
 class App extends Component {
-  render() {
 
-    return (
-      <div className="App">
-        <div className="img-">
-          <a className="img-" href="/">
-            <img className="img" src="https://www.yuxiuk.com/images/activities/uk-china-summit-2017.jpg"/>
-          </a>
-        </div>
-        <div>
-          HomeZh
 
-          +++++++++++++++++++++++++++++++++++++++++
-          +++++++++++++++++++++++++++++++++++++++++
-        </div>
-      </div>
-    );
-  }
+    render() {
+
+        return (
+            <div className="App">
+                <div className="img-">
+                    <a className="img-" href="/">
+                        <img className="img" src="https://www.yuxiuk.com/images/activities/uk-china-summit-2017.jpg"/>
+                    </a>
+                </div>
+
+                {/*<div className="container">*/}
+                {/*    <div className="row">*/}
+                {/*        <div className="col-6 col-md-4 card">*/}
+                {/*            asdfsadfasdfdsaf*/}
+                {/*        </div>*/}
+
+                {/*        <div className="col-1 card">*/}
+                {/*            adsfasdfdsafsadf*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+
+
+                <div className="card-container">
+                    <div className="card">
+                        jalkdsjf;akf;jsakf;ldsadd
+                    </div>
+                    <div className="card">
+                        jalkdsjf;akf;jsakf;ldsadd
+                    </div>
+                    <div className="card">
+                        jalkdsjf;akf;jsakf;ldsadd
+                    </div>
+                </div>
+
+
+            </div>
+        );
+    }
 }
 
 export default App;
